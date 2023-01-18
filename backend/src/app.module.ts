8e11179dad845f5e348bd1e1fda8679b100119d4
Hoonjo <jaiemf@naver.com>
2023-01-19 01:52:31 +0900

    refactor: Remove app.service and controller when created initialize nest

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectModule } from './project/project.module';
 
 @Module({
   imports: [PrismaModule, ProjectModule, PostModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
