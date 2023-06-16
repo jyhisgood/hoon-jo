e45fe6d5198d045c112e19ea4b691244a56fae3d
Hoonjo <jaiemf@naver.com>
2023-06-16 18:57:43 +0900

    conf: Add google fonts with tailwind on nextjs

--- frontend/components/layouts/MainLayout.tsx
+++ frontend/components/layouts/MainLayout.tsx
@@ -3,17 +3,33 @@ import Body from './Body';
 import Footer from './Footer';
 import Header from './Header';
 
+import { Roboto, Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKr = Noto_Sans_KR({
+  preload: false,
+  weight: ['100', '400', '700', '900'],
+});
+
+const roboto = Roboto({
+  preload: false,
+  weight: ['100', '400', '700', '900'],
+  variable: '--roboto',
+});
+
 type Props = {
   children: ReactNode;
 };
 
 const MainLayout = ({ children }: Props) => {
+  const cls = (...classnames: string[]) => {
+    return classnames.join(' ');
+  };
   return (
-    <>
+    <main className={cls(notoSansKr.className, roboto.variable)}>
       {/* <Header /> */}
       <Body>{children}</Body>
       {/* <Footer /> */}
-    </>
+    </main>
   );
 };
 
