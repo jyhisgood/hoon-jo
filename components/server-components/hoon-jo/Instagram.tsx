2855d74ff0ca0dbcf62fb6eb4497a9c0946af327
jyhisgood <jaiemf@gmail.com>
2023-08-16 22:09:21 +0900

    style: Add responsive hoon-jo page

--- components/server-components/hoon-jo/Instagram.tsx
+++ components/server-components/hoon-jo/Instagram.tsx
@@ -11,17 +11,17 @@ const Instagram = async ({ promise }: any) => {
   return (
     <>
       <div
-        className="h-[300px] bottom-[130px]  absolute left-0 flex-wrap"
+        className="h-[120px] sm:h-[200px] md:h-[250px] lg:h-[300px] bottom-[130px]  absolute left-0 flex-wrap"
         style={{
           transform: 'rotate(-23deg)',
           width: 'calc(100% + 400px)',
         }}
       >
-        <div className="relative inline-flex gap-[20px]">
+        <div className="relative inline-flex gap-[7px] md:gap-[12px]">
           {instagram1?.map((item, key: number) => (
             <div
               key={key}
-              className="w-[300px] h-[300px] border-2 overflow-hidden border-black rounded-full flex"
+              className="w-[120px] sm:w-[200px] md:w-[250px] lg:w-[300px] h-[120px] sm:h-[200px] md:h-[250px] lg:h-[300px] border-2 overflow-hidden border-black rounded-full flex"
             >
               <Image
                 src={item.media_url}
@@ -42,11 +42,11 @@ const Instagram = async ({ promise }: any) => {
           width: 'calc(100% + 400px)',
         }}
       >
-        <div className="relative inline-flex gap-[20px]">
+        <div className="relative inline-flex gap-[7px] md:gap-[12px]">
           {instagram2?.map((item, key: number) => (
             <div
               key={key}
-              className="w-[300px] h-[300px] border-2 overflow-hidden border-black rounded-full flex"
+              className="w-[120px] sm:w-[200px] md:w-[250px] lg:w-[300px] h-[120px] sm:h-[200px] md:h-[250px] lg:h-[300px] border-2 overflow-hidden border-black rounded-full flex"
             >
               <Image
                 src={item.media_url}
